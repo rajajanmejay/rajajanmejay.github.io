c0b93e295d98dbf98fa45ad66535de2328056232
rajajanmejay <rajajanmejaya@gmail.com>
2025-07-16 17:02:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My PhD Website</title>
+</head>
+<body>
+  <h1>Welcome to my PhD website!</h1>
+  <p>Hosted on my Windows 11 machine with XAMPP.</p>
+</body>
+</html>
